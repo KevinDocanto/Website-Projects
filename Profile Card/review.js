45241e48a8d38070195b645828b1770cb6dfdd65
Ogrinho <kevin-teus@hotmail.com>
2021-10-20 00:08:16 -0400

    Add files via upload

---
+++ Profile Card/review.js
@@ -0,0 +1,9 @@
+class Review {
+  constructor(name, job, description, img) {
+    this.name = name;
+    this.job = job;
+    this.description = description;
+    this.img = img;
+    this.id++;
+  }
+}
