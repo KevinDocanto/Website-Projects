73f1358a284f605181ae3999d845d528caffa706
Ogrinho <kevin-teus@hotmail.com>
2021-11-10 18:42:14 -0500

    Adding files to weatherAPI

--- Profile Card/review.js
+++
@@ -1,9 +0,0 @@
-class Review {
-  constructor(name, job, description, img) {
-    this.name = name;
-    this.job = job;
-    this.description = description;
-    this.img = img;
-    this.id++;
-  }
-}
