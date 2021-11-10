3ec21762328b3ece7dc383475d602800d3391d79
Ogrinho <kevin-teus@hotmail.com>
2021-11-10 17:37:43 -0500

    Added feature that tells today's date

--- Calendar/script.js
+++ Calendar/script.js
@@ -1,21 +1,45 @@
 const row = $(".row");
+const day = $(".day");
+const date = new Date();
+let key = -1;
 
 row.click(function () {
-  if ($(this).find(".mark").length > 0)
-    $(this)[0].removeChild($(this)[0].firstChild);
+  if ($(this).find(".mark").length > 0) return removeMark(this);
   else {
-    let mark = document.createElement("div");
-    let x = document.createElement("h1");
-    x.textContent = "X";
-    mark.classList.add("mark");
-    mark.append(x);
-    this.prepend(mark);
-
-    document.cookie = "name=kevin;";
-
-    console.log(this);
+    insertMark(this);
+    let child = this.lastChild;
+    localStorage.setItem(++key, child.textContent);
   }
-
-  let d = row;
-  console.log("testing");
 });
+
+function createMark() {
+  let mark = document.createElement("div");
+  let x = document.createElement("h1");
+  x.textContent = "X";
+  mark.classList.add("mark");
+  mark.append(x);
+
+  return mark;
+}
+
+function insertMark(row) {
+  return row.prepend(createMark());
+}
+
+function removeMark(row) {
+  return $(row)[0].removeChild($(row)[0].firstChild);
+}
+
+function getDay() {
+  return (day[date.getDate() - 1].style.backgroundColor = "lightgray");
+}
+
+function saveMark() {
+  for (let i = localStorage.length; i >= key; i--) {
+    day[localStorage.getItem(i) - 1].prepend(createMark());
+    console.log(i);
+  }
+}
+
+getDay();
+saveMark();
